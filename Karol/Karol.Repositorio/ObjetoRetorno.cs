155bcafeeaeadbad5679bf24855a4d6b48cf2c8f
Cleyton Ferrari <cleyton@w7br.com>
2013-05-27 16:44:03 -0400

    Teste do VS2012

---
+++ Karol/Karol.Repositorio/ObjetoRetorno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karol.Repositorio
+{
+    class ObjetoRetorno <T> where T : class
+    {
+
+    }
+}
